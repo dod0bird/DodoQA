3f214ae4433f117af6d2fa13af177e52362040d1
Emily Tu <mrdodobird@icloud.com>
2021-01-10 16:39:41 -0700

    Hello world javascript

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/')
 def hello_world():
     return app.send_static_file('index.html')
+
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
