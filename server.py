ba4e730b065dbaa0ba483e25d8aed5a1ff38b510
Emily Tu <mrdodobird@icloud.com>
2021-01-09 22:08:26 -0700

    add basic bootstrap template

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return app.send_static_file('index.html')
