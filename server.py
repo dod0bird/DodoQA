3bfe5b638064b24165367bf2a4ea3834d0ac59f9
Emily Tu <mrdodobird@icloud.com>
2021-01-10 17:47:29 -0700

    send question to server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, jsonify, request, send_from_directory
 app = Flask(__name__, static_url_path='/static')
 
 
@@ -10,3 +10,14 @@ def hello_world():
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('js', path)
+
+
+@app.route('/api/ask', methods=['POST'])
+def ask_endpoint():
+    request_json = request.get_json()
+    print(request_json)
+    answers = [
+        "Felix Arvid Ulf Kjellberg, known online as PewDiePie, is a Swedish YouTuber and comedian, known primarily for his Let's Play videos and comedic formatted shows.",
+        "PewDiePie is one of the most popular YouTube personalities in the world, earning an estimated $13 million in 2019 alone. PewDiePie, aka Felix Kjellberg, got his start doing gaming walkthroughs and reviews, but has since expanded to more satirical commentary and meme roundups."
+    ]
+    return jsonify(answers)
--- static/js/dodoqa.js
+++ static/js/dodoqa.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
-    console.log("Hello World!");
-});
+    $('#question-button').click(function() {
+        var question = $('#question-field').val();
+        console.log(question);
 
+        $.ajax({
+            type: 'POST',
+            url: '/api/ask',
+            data: JSON.stringify({question: question}),
+            contentType: 'application/json',
+            dataType: 'json'
+        }).done(function(response) {
+            console.log('response: ' + response);
+        }).fail(function(jqXHR, textStatus) {
+            console.log("Request failed: " + textStatus);
+        });
+    });
+});
